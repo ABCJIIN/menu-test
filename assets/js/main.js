08aa41eb7db1de4605c5b5aa37b4e34573ee8b13
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-05-04 14:27:43 +0900

    0504/14:27/jinny

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
-   $(".list-item").click(function(){
-   $(this).next("div").slideToggle(200);
-   });
-});
-
-// $(document).ready(function(){
-//    $('li').click(function(){
-//       $('li').removeClass()
-//          $(this).addClass('on')
-//    });
-// });
+   $('.list-item').click(function(){
+      $(this).first().nextAll('div').slideUp(200);
+      if($(this).next('div').is(':hidden')) {
+         $(this).next("div").slideDown(200);
+      } else if ($(this).next('div').is(':visible')) {
+         $(this).next('div').slideUp(200);
+      } else {
+      }
+   })
+})
